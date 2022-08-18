bc4fc32f0fb2f4566ebc4a9e50bba4afe3d6a17e
Leo Rapini <leorapini@gmail.com>
2022-08-18 08:39:55 -0300

    add api key to Nasa url generator

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,7 +1,7 @@
 // Nasa API docs https://github.com/nasa/apod-api
 class NasaUrl {
   static const String apiUrl = 'https://api.nasa.gov/planetary/apod';
-  static const String apiKey = '';
+  static const String apiKey = 'vwEGRVVekBVT8GXSzPpBHdG3GeTJ0DwRdZDcXLec';
   static String getUrlByDate(
           {required String startDate, required String endDate}) =>
       '$apiUrl?api_key=$apiKey&start_date=$startDate&end_date=$endDate';
