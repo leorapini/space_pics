91a04e2114fe12117667e63db82396eac8406e3f
Leo Rapini <leorapini@gmail.com>
2022-08-19 21:01:29 -0300

    refactor helper functions

--- lib/helpers/errors.dart
+++ lib/helpers/errors.dart
@@ -1,3 +1,6 @@
+// Simple implementation to differentiate between errors derived from
+// external API (i.e. ServerError from NASA api) or local ones (i.e. 
+// LocalError derived from corruped json file).
 class ServerError implements Exception {}
 
 class LocalError implements Exception {}
--- lib/helpers/format_functions.dart
+++ lib/helpers/format_functions.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 import '../data/constants/values.dart';
 
 // Returns a String with the following for farmat YYYY-MM-DD as per Nasa's Api
+// Nasa API docs https://github.com/nasa/apod-api
 String fromDateTimeToString(DateTime date) {
   final DateFormat formatter = DateFormat('yyyy-MM-dd');
   final String formattedDate = formatter.format(date);
@@ -15,7 +16,6 @@ DateTime daysAgo(DateTime date) {
 }
 
 bool isDateValid(String value) {
-  // add check date after 1995-06-16 and before DateTime.now()
   final result = DateTime.tryParse(value) != null;
   return result;
 }
